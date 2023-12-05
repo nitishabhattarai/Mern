e37b41d647e53f17b4f642fd6e045382001f277e
Nitisha Bhattarai <nitishaabhattarai@gmail.com>
2023-12-05 21:29:18 +0545

    handling errors

--- client/src/components/WorkoutForm.js
+++ client/src/components/WorkoutForm.js
@@ -7,6 +7,7 @@ const WorkoutForm = () => {
   const [load, setLoad] = useState("");
   const [reps, setReps] = useState("");
   const [error, setError] = useState(null);
+  const [emptyFields, setEmptyFields] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -24,12 +25,14 @@ const WorkoutForm = () => {
 
     if (!response.ok) {
       setError(json.error);
+      setEmptyFields(json.emptyFields);
     }
     if (response.ok) {
-      setError(null);
-      setLoad("");
       setTitle("");
+      setLoad("");
       setReps("");
+      setError(null);
+      setEmptyFields([]);
       console.log("New workout added!", json);
       dispatch({ type: "CREATE_WORKOUT", payload: json });
     }
@@ -43,6 +46,7 @@ const WorkoutForm = () => {
         type="text"
         onChange={(e) => setTitle(e.target.value)}
         value={title}
+        className={emptyFields.includes('title') ? 'error':''}
       />
 
       <label> Load (in kg):</label>
@@ -50,6 +54,7 @@ const WorkoutForm = () => {
         type="text"
         onChange={(e) => setLoad(e.target.value)}
         value={load}
+        className={emptyFields.includes('load') ? 'error':''}
       />
 
       <label>Reps:</label>
@@ -57,6 +62,7 @@ const WorkoutForm = () => {
         type="text"
         onChange={(e) => setReps(e.target.value)}
         value={reps}
+        className={emptyFields.includes('reps') ? 'error':''}
       />
       <button>Add Workout</button>
       {error && <div className="error">{error}</div>}
